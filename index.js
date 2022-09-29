ad725e2ae2b3a2cb1195c2c636317a21e7ee1644
jmg <macgreene14@gmail.com>
2022-09-29 10:32:40 -0400

    Added Update Role functionality

--- index.js
+++ index.js
@@ -67,11 +67,11 @@ async function accessDB(action) {
           {
             type: "input",
             name: "department",
-            message: "Input department of role",
+            message: "Input department id of role",
           },
         ]);
         insert = await db.query(
-          `INSERT INTO role (id, title, salary, department_id) VALUES (default, ?, ?, ?)`, // default, 0, or NULL should work as place holder for autoincrement id
+          `INSERT INTO role (id, title, salary, department_id) VALUES (default, ?, ?, ?)`,
           [response.name, response.salary, response.department]
         );
         results = await db.query("SELECT * FROM role");
@@ -92,7 +92,7 @@ async function accessDB(action) {
           {
             type: "input",
             name: "role",
-            message: "Input role of employee",
+            message: "Input role id of employee",
           },
           {
             type: "input",
@@ -109,11 +109,32 @@ async function accessDB(action) {
             response.manager,
           ]
         );
-        results = await db.query("SELECT * FROM department");
+        results = await db.query("SELECT * FROM employee");
         console.table(results[0]);
         break;
       case "Update an Employee Role":
-        console.log("update - Update an Employee Role");
+        // UPDATE employee SET role = "new role" WHERE name = "selected name"
+        // WHEN I choose to update an employee role
+        // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+        response = await inquirer.prompt([
+          {
+            type: "input",
+            name: "employee",
+            message: "Input an employee id to update their role",
+          },
+          {
+            type: "input",
+            name: "role",
+            message: "Input employee's new role id",
+          },
+        ]);
+        insert = await db.query(
+          `UPDATE employee SET role_id = ? WHERE id = ?`,
+          [response.role, response.employee]
+        );
+        results = await db.query("SELECT * FROM employee");
+        console.table(results[0]);
+
         break;
       default:
         console.log("Please select an action");
@@ -150,22 +171,3 @@ function main() {
 }
 
 main();
-
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
